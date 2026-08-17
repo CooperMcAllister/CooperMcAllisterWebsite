f9baaf76746435dc123665e3a7bb7a5287fb1ce9
Cooper McAllister <coopamcallister@gmail.com>
2026-08-17 10:50:56 -0500

    Header removed .html

--- header.js
+++ header.js
@@ -2,9 +2,9 @@ const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
   <nav>
     <ul>
-      <li><a href="index.html">Home</a></li>
-      <li><a href="portfolio.html">Portfolio</a></li>
-      <li><a href="resume.html">Resume</a></li>
+      <li><a href="index">Home</a></li>
+      <li><a href="portfolio">Portfolio</a></li>
+      <li><a href="resume">Resume</a></li>
     </ul>
   </nav>
 `;
